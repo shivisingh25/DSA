7c504bd5ebf30aa05cd051f9d3301b6dd14e7adf
Shivangi Singh <shivangisingh0306@gmail.com>
2025-07-23 18:55:39 +0530

    Left Rotate the Array By One Place

---
+++ Arrays/LeftRotateArray.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    int temp = arr[0];
+    for(int i=0;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    arr[n-1]=temp;
+
+    for(int i=0;i<n;i++){
+        cout <<  arr[i] << " ";
+    }
+
+    return 0;
+}
