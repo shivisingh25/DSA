85ff133a0d458724925523b8cc0516aadfcd7c7a
Shivangi Singh <shivangisingh0306@gmail.com>
2025-08-02 22:23:33 +0530

    Implement Upper Bound

--- BinarySearch/LowerBound.cpp
+++ BinarySearch/LowerBound.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Find Lower Bound
 int lowerBound(vector<int> &nums, int x){
         int low = 0;
         int high = nums.size()-1;
@@ -9,6 +10,25 @@ int lowerBound(vector<int> &nums, int x){
         while(low <= high){
             int mid = (low + high)/2;
             if(nums[mid] >= x){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+//Find Upper Bound
+ int upperBound(vector<int> &nums, int x){
+        int low = 0;
+        int high = nums.size()-1;
+        int ans = nums.size();
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] > x){
                 ans = mid;
                 high = mid - 1;
             }
@@ -32,6 +52,7 @@ int main(){
     int x;
     cin >> x;
 
-    cout << "Lower Bound at Index: " << lowerBound(arr, x);
+    cout << "Lower Bound at Index: " << lowerBound(arr, x) << endl;
+    cout << "Upper Bound at Index: " << upperBound(arr,x);
     return 0;
 } 
