539a8b0b4f1f58042485938c4acc72df2d5b7685
Shivangi Singh <shivangisingh0306@gmail.com>
2025-08-13 12:18:34 +0530

    Conversion Arr Into LL, Length Of LL, Traversal in LL, Search Element in LL

--- LinkedList/Example.cpp
+++ LinkedList/Example.cpp
@@ -10,10 +10,48 @@ struct Node {
         data = data1;
         next = next1;
     }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
 };
 
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+int lengthOfLL(Node* head){
+    int cnt = 0;
+    Node* temp = head;
+    while(temp){
+        temp = temp->next;
+        cnt++;
+    }
+    return cnt;
+}
+
+int checkIfPresent(Node* head, int val){
+    Node* temp = head;
+    while(temp){
+        if(temp->data == val) return 1;
+        temp = temp->next;
+        
+    }
+    return 0;
+}
+
 int main(){
-    vector<int> arr = { 2, 5, 8, 7};
+    vector<int> arr = { 12, 5, 8, 7};
 
     // Node* y = new Node(arr[0], nullptr);
     // cout << y;
@@ -22,8 +60,29 @@ int main(){
     // cout << y.data << endl;
     // cout << y.next;
 
-    Node* y = new Node(arr[0], nullptr);
-    cout << y->data;
+    // Node* y = new Node(arr[0], nullptr);
+    // cout << y->data;
 
+
+    // Array in Linked List
+    // Node* head = convertArr2LL(arr);
+    // cout << head->data;
+
+    
+    //Traversal in Linked List
+    // Node* head = convertArr2LL(arr);
+    // Node* temp = head;
+    // while(temp){
+    //     cout << temp->data << " ";
+    //     temp = temp->next;
+    // }
+
+    //Length of Linked List
+    // Node* head = convertArr2LL(arr);
+    // cout << lengthOfLL(head);
+
+    //Search an Element in Linked List
+    Node* head = convertArr2LL(arr);
+    cout << checkIfPresent(head, 5);
     return 0;
 }
