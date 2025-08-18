5532377683dc50821634890a0bcecd784e9a933a
Shivangi Singh <shivangisingh0306@gmail.com>
2025-08-18 10:29:05 +0530

    Deletion of different cases in DLL

--- DoublyLinkedList/DLL.cpp
+++ DoublyLinkedList/DLL.cpp
@@ -33,6 +33,96 @@ Node* convertArr2DLL(vector<int> &arr){
     return head;
 }
 
+Node* deleteHead(Node* head){
+    if(head == NULL || head->next == NULL){
+        return NULL;
+    }
+     
+    Node* prev = head;
+    head = head->next;
+
+    head->back = nullptr;
+    prev->next = nullptr;
+    
+    delete prev;
+    return head;
+}
+
+
+Node* deleteTail(Node* head){
+    if(head == NULL || head->next == NULL){
+        return NULL;
+    }
+     
+    Node* tail = head;
+    while(tail->next != nullptr){
+        tail = tail->next;
+    }
+    Node* prev = tail->back;
+    tail->back = nullptr;
+    prev->next = nullptr;
+    delete tail;
+    return head;
+}
+
+Node* deleteKNode(Node* head, int k){
+    if(head == NULL){
+        return NULL;
+    }
+
+    int cnt = 0;
+    Node* temp = head;
+    
+    while(temp != nullptr){
+        cnt++;
+
+        if(cnt == k){
+            break;
+        }
+
+        temp = temp->next;
+    }
+
+    Node* prev = temp->back;
+    Node* front = temp->next;
+
+    if(prev == NULL && front == NULL){
+        return NULL;
+    }
+    else if(prev == nullptr){
+       return deleteHead(head);
+    }
+    else if(front == nullptr){
+       return deleteTail(head);
+    }
+    prev->next = front;
+    front->back = prev;
+
+    temp->next = nullptr;
+    temp->back = nullptr;
+    delete temp;
+
+    return head;
+}
+
+void deleteNode(Node*temp){
+    Node* prev = temp->back;
+    Node* front = temp->next;
+
+    if(front == NULL){
+        prev->next = nullptr;
+        temp->back = nullptr;
+        free(temp);
+        return;
+    }
+
+    prev->next = front;
+    front->back = prev;
+
+    temp->next = temp->back = nullptr;
+    free(temp);
+}
+
 void print(Node* head){
     while(head != nullptr){
         cout << head->data << " ";
@@ -44,7 +134,27 @@ int main(){
     vector<int> arr = { 12, 5, 8, 7};
 
     // Convert Array into Doubly Linked List
+    // Node* head = convertArr2DLL(arr);
+    // print(head);
+
+    // Delete Head Node
+    // Node* head = convertArr2DLL(arr);
+    // head = deleteHead(head);
+    // print(head);
+
+    // // Delete Tail Node
+    // Node* head = convertArr2DLL(arr);
+    // head = deleteTail(head);
+    // print(head);
+
+    // // Delete Kth Node
+    // Node* head = convertArr2DLL(arr);
+    // head = deleteKNode(head,3);
+    // print(head);
+
+    // Delete Node
     Node* head = convertArr2DLL(arr);
+    deleteNode(head->next->next);
     print(head);
 
     return 0;
